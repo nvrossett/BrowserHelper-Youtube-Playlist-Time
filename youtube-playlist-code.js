720db967c9cd50a8e8c899dd6e2a9d6d2392bd8a
Nicolas Rossett <nicolas@nvieira.com.br>
2018-05-30 19:38:18 -0300

    Playlist in multiples speeds

--- youtube-playlist-code.js
+++ youtube-playlist-code.js
@@ -1,35 +1,63 @@
 (function() {
 
-    var timeSeconds = 0,
-        timestampDivList = document.querySelectorAll(".ytd-playlist-video-list-renderer .ytd-thumbnail-overlay-time-status-renderer")  
-          
+    var timeSeconds = 0, 
+        timeSecondsInAnotherSpeed = 0,
+        playlist = document.querySelectorAll(".ytd-item-section-renderer ytd-playlist-video-renderer.ytd-playlist-video-list-renderer"),
+        list = [],
+        speed = 1.25
 
-    for(var i = 0; i < timestampDivList.length; i++) {
-        var timestampDiv = timestampDivList[i],
-            timeStr = timestampDiv.innerHTML
+    function secondsIntime( timeSeconds ){
+        var hours = ( parseInt((timeSeconds / 60) / 60) > 9 ) ? parseInt((timeSeconds / 60) / 60) : '0'+parseInt((timeSeconds / 60) / 60)
+        var minutes = ( parseInt((timeSeconds / 60) % 60) > 9 ) ? parseInt((timeSeconds / 60) % 60) : '0'+parseInt((timeSeconds / 60) % 60)
+        var seconds = ( parseInt((timeSeconds % 60)) > 9 ) ? parseInt((timeSeconds % 60)) : '0'+parseInt((timeSeconds % 60))
 
-        if ( timeStr !== '' ){
-            var seconds,
-                timeParts = timeStr.split(":"),                
-
-            switch ( timeParts.length ) {
-                case 1:     seconds = parseInt(timeParts[0]);                                                                               break;
-                case 2:     seconds = (parseInt(timeParts[0]) * 60) + parseInt(timeParts[1]);                                               break;
-                case 3:     seconds = ((parseInt(timeParts[0]) * 60) * 60) + (parseInt(timeParts[1]) * 60) + parseInt(timeParts[2]);        break;                    
-            }
-
-            timeSeconds += seconds;
-        }
-    }
-
-    var hours = parseInt((timeSeconds / 60) / 60),
-        minutes = parseInt((timeSeconds / 60) % 60),
-        seconds = parseInt((timeSeconds % 60))
+        return { hours : hours, minutes : minutes, seconds : seconds }
+    }   
 
     function plural( label, time ){
         return (time>1) ? label+'s' : label;
+    }        
+
+    for(var i = 0; i < playlist.length; i++) {
+        var video = playlist[i],
+            title = video.querySelector('h3').innerText.trim(),
+            time = video.querySelector('.ytd-thumbnail-overlay-time-status-renderer').innerHTML.trim()
+
+        if ( time !== '' ){
+            var seconds,
+                timeParts = time.split(":"),
+                theTime
+                
+                switch ( timeParts.length ) {
+                    case 1:     seconds = parseInt(timeParts[0]);                                                                               break;
+                    case 2:     seconds = (parseInt(timeParts[0]) * 60) + parseInt(timeParts[1]);                                               break;
+                    case 3:     seconds = ((parseInt(timeParts[0]) * 60) * 60) + (parseInt(timeParts[1]) * 60) + parseInt(timeParts[2]);        break;                    
+                }
+                
+            var currentTime = secondsIntime( seconds / speed )
+
+            if ( currentTime.hours == 0 &&  currentTime.minutes == 0 &&  currentTime.seconds > 0 ){
+                theTime = '00:'+currentTime.seconds
+            } else if ( currentTime.hours == 0 &&  currentTime.minutes > 0 &&  currentTime.seconds > 0 ){
+                theTime = currentTime.minutes+':'+currentTime.seconds
+            } else if ( currentTime.hours > 0 &&  currentTime.minutes > 0 &&  currentTime.seconds > 0 ){
+                theTime = currentTime.hours+':'+currentTime.minutes+':'+currentTime.seconds
+            }  
+
+            list.push({ 'Title': title, 'Time': time, 'Speed': speed, 'Time in Speed': theTime })
+
+
+            timeSeconds += seconds;
+            timeSecondsInAnotherSpeed += (seconds / speed);
+        }
     }
 
-    alert(hours + " " + plural('hour', hours) +', ' + minutes + " " + plural('minute', minutes) + ' ' + "and" +' ' + seconds + " " + plural('second', seconds));
+    console.table( list )
+
+    var currentTime = secondsIntime( timeSeconds )
+    console.log('Normal Speed:', currentTime.hours + " " + plural('hour', currentTime.hours) +', ' + currentTime.minutes + " " + plural('minute', currentTime.minutes) + ' ' + "and" +' ' + currentTime.seconds + " " + plural('second', currentTime.seconds));
+
+    var currentTimeInSpeed = secondsIntime( timeSecondsInAnotherSpeed )
+    console.log('Speed ('+speed+'):',  currentTimeInSpeed.hours + " " + plural('hour', currentTimeInSpeed.hours) +', ' + currentTimeInSpeed.minutes + " " + plural('minute', currentTimeInSpeed.minutes) + ' ' + "and" +' ' + currentTimeInSpeed.seconds + " " + plural('second', currentTimeInSpeed.seconds));
 
 })();
